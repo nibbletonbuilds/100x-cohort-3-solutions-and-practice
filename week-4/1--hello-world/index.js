ef580663526403be342ace18f1067499f6ff355a
nibbletonbuilds <nibbletondhamster@gmail.com>
2025-06-13 11:26:39 +0530

    finish 1--hello-world

---
+++ week-4/1--hello-world/index.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+
+const app = express()
+
+app.get ('/', (req, res) => {
+    res.send ('hello, world!');
+})
+
+app.listen (3000, '0.0.0.0', () => {
+    console.log ('server running @ port 3000')
+});
